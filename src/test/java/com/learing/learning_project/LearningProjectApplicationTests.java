3eae8d07369f94abdfeaa52fc9fe3c0293fc1363
daile269 <daile2692003@gmail.com>
2025-04-17 13:53:13 +0700

    init project

---
+++ src/test/java/com/learing/learning_project/LearningProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learing.learning_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
